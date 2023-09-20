cdd2aa2505083479c44620af18d9cf922495df69
Isaac Johnson <isaac@socialsimple.io>
2023-09-20 19:13:42 +1000

    feat: time card component for rendering the number map

---
+++ src/components/timeCard.tsx
@@ -0,0 +1,50 @@
+import {
+  EnvelopeIcon,
+  PauseIcon,
+  PhoneIcon,
+  PlayIcon,
+} from "@heroicons/react/20/solid";
+
+interface ITimeCardProps {
+  time: number;
+  step: number;
+  children: React.ReactNode;
+}
+
+export default function TimeCard({ time, step, children }: ITimeCardProps) {
+  return (
+    <div className="col-span-1 mb-4  rounded-lg bg-white shadow">
+      <div className="py-2x flex items-center justify-between bg-indigo-500 px-4">
+        <h4 className="font-bold text-white">Frequency Map</h4>
+        <div className="flex items-center justify-center">
+          <h4 className="font-bold text-white">Time remaining until update</h4>
+          <div className="my-2 ml-2 flex h-8  w-8 flex-shrink-0 grid-cols-2 items-center justify-center rounded-full bg-indigo-200 font-bold text-indigo-800">
+            {time}
+          </div>
+        </div>
+      </div>
+
+      <div className="grid space-x-6 p-6">
+        <div className="col-span-10 flex flex-wrap items-center">
+          {children}
+        </div>
+      </div>
+      <div>
+        <div className="-mt-px flex divide-x divide-indigo-700">
+          <div className="flex w-0 flex-1 border-r-2">
+            <button className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent bg-indigo-500 py-4 text-sm font-semibold text-white  hover:bg-indigo-800">
+              <PauseIcon className="h-5 w-5 text-white" aria-hidden="true" />
+              Pause
+            </button>
+          </div>
+          <div className="-ml-px flex w-0 flex-1">
+            <button className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent bg-indigo-500 py-4 text-sm font-semibold text-white hover:bg-indigo-800">
+              <PlayIcon className="h-5 w-5 text-white" aria-hidden="true" />
+              Resume
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
